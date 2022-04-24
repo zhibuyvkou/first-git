e77448f5888fb3177132189b6b0bb5385dacf981
gsd <guoshuaida0329@163.com>
2022-04-24 10:16:44 +0800

    zhangmazi commit

--- hello.java
+++ hello.java
@@ -25,3 +25,17 @@ sout(123456)
 sout(0987654321)
 ArrayList<String> list = new ArrrayList<>();
 System.out.plintln(wgssb)
+for(int i=0;i<100;i++){
+  if(i%2==0){
+System.out.println("什么TM的是偶数")
+System.out.println("什么TM的是偶数")
+System.out.println("什么TM的是TMD偶数")
+System.out.println("翻译翻译")
+System.out.println("偶数嘛，i就是偶数")
+
+
+  }
+}
+
+
+	
